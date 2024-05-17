e27bed5ada2e41b6755d46adad18d71e9e32beb3
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-05-17 19:37:49 +0600

    updating project

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -2,48 +2,49 @@ import { useEffect, useState } from "react";
 import Container from "../Shared/Container";
 import RoomsCard from "./RoomsCard";
 import Heading from "../Shared/Heading";
+import Categories from "../Categories/Categories";
 
-const Rooms = () => {   
+const Rooms = () => {
 
-    const [rooms, setRooms] = useState([])
+  const [rooms, setRooms] = useState([])
 
-    useEffect(() => 
-        {
+  useEffect(() => {
 
-            fetch("./rooms.json")
-            .then(res => res.json())
-            .then(data => setRooms(data))
-        }
-        
-        ,[])
+    fetch("./rooms.json")
+      .then(res => res.json())
+      .then(data => setRooms(data))
+  }
 
-    return (
-        <div>
-            <Container>
-             {/* {
+    , [])
+
+  return (
+    <div>
+        <Categories/>
+      <Container>
+        {/* {
                 rooms.map(room => (
                     <RoomsCard key={room?._id} room = {room} ></RoomsCard>
                 ))
              } */}
-              {rooms && rooms.length > 0 ? (
-        <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3'>
-          {rooms.map(room => (
-            <RoomsCard key={room._id} room={room} />
-          ))}
-        </div>
-      ) : (
-        <div className='flex items-center justify-center min-h-[calc(100vh-300px)]'>
-          <Heading
-            center={true}
-            title='No Rooms Available In This Category!'
-            subtitle='Please Select Other Categories.'
-          />
-        </div>
-      )}
-                
-            </Container>
-        </div>
-    );
+        {rooms && rooms.length > 0 ? (
+          <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3'>
+            {rooms.map(room => (
+              <RoomsCard key={room._id} room={room} />
+            ))}
+          </div>
+        ) : (
+          <div className='flex items-center justify-center min-h-[calc(100vh-300px)]'>
+            <Heading
+              center={true}
+              title='No Rooms Available In This Category!'
+              subtitle='Please Select Other Categories.'
+            />
+          </div>
+        )}
+
+      </Container>
+    </div>
+  );
 };
 
 export default Rooms;
