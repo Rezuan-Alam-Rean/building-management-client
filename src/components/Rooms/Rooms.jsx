12a8b4feaf208384beea4064f028030a26e01b7f
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-11-07 21:56:39 +0600

    update

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -45,10 +45,11 @@ const Rooms = () => {
             ))}
           </div>
         ) : (
-          <div className='flex items-center justify-center min-h-[calc(100vh-300px)]'>
+          <div className='flex items-center text-white justify-center min-h-[calc(100vh-300px)]'>
             <Heading
               center={true}
-              title='No Rooms Available In This Category!'
+              title='No Rooms are  Available In This Category!'
+              
               subtitle='Please Select Other Categories.'
             />
           </div>
